9b2d9400e8aadcf88c6306079cf717f66b6b02bf
krishnakant20 <89294839+krishnakant20@users.noreply.github.com>
2022-02-20 19:22:46 +0530

    4 Project Statement for Placing the Order

--- src/components/orders/Orders.js
+++ src/components/orders/Orders.js
@@ -1,9 +1,66 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Orders.css'
+
 
 const Orders = () => {
+
     return (
         <>
-            <h1>Orders</h1>
+            <h1>Fast Orders Delivery </h1>
+            <div className='flexContainer my-5'>
+
+                <div className="card" style={{ "width": "18rem" }}>
+                    <h3>Review Item</h3>
+                    <img src={require("../../assests/images/2.jpg")} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <Link to="/orders" className="btn btn-success">Place Order</Link>
+                    </div>
+                </div>
+
+                <div className="card" style={{ "width": "18rem" }}>
+                    <h3>Address</h3>
+                    <form>
+                        <div className="mb-3">
+                            {/* <label for="name" className="form-label">First Name</label> */}
+                            <input type="text" className="form-control" id="firstname" placeholder="Name" />
+                        </div>
+                        <div className="mb-3">
+                            {/* <label for="exampleInputEmail1" className="form-label">Email address</label> */}
+                            <input type="number" className="form-control" id="phone" placeholder="Mobile Number" />
+                        </div>
+                        <div className="mb-3">
+                            {/* <label for="exampleInputEmail1" className="form-label">Email address</label> */}
+                            <input type="text" className="form-control" id="street" placeholder="Street" />
+                        </div>
+                        <div className="mb-3">
+                            {/* <label for="exampleInputEmail1" className="form-label">Email address</label> */}
+                            <input type="text" className="form-control" id="city" placeholder="City" />
+                        </div>
+                        <div className="mb-3">
+                            {/* <label for="exampleInputEmail1" className="form-label">Email address</label> */}
+                            <input type="text" className="form-control" id="state" placeholder="State" />
+                        </div>
+                        <div className="mb-3">
+                            {/* <label for="exampleInputEmail1" className="form-label">Email address</label> */}
+                            <input type="text" className="form-control" id="landmark" placeholder="Landmark" />
+                        </div>
+                        <div className="mb-3">
+                            {/* <label for="exampleInputEmail1" className="form-label">Email address</label> */}
+                            <input type="number" className="form-control" id="zipcode" placeholder="Zipcode" />
+                        </div>
+
+                        <button type="submit" className="btn btn-success">Add Address</button>
+                    </form>
+                </div>
+                <div className="card" style={{ "width": "18rem" }}>
+                    <h3>Place your Order</h3>
+                    <Link to="#" className="btn btn-success">Place Order</Link>
+                </div>
+            </div>
+
         </>
     )
 }
